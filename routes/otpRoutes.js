108a122196696793a043a5bfb58b1d668cc18e6d
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-08 23:37:03 +0200

    added verify and delete email otp controller and services

--- routes/otpRoutes.js
+++ routes/otpRoutes.js
@@ -1,6 +1,11 @@
 import express from "express";
 
+import { createAndSendEmailOtp, verifyEmailOtp } from "../controllers/otpControllers.js";
+
 const otpRoutes = express.Router();
+
+otpRoutes.post("/send-email-otp", createAndSendEmailOtp);
+otpRoutes.post("/verify-email-otp", verifyEmailOtp);
 
 
 export default otpRoutes;
