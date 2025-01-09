4c9a47c2fb7aacb3a046c44e410638e45226fd5c
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-09 22:00:12 +0200

    added separate multer middleware for client files uploads and provider file upload

--- middlewares/multer.js
+++ middlewares/multer.js
@@ -11,3 +11,13 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({ storage: storage });
+
+export const uploadClientFiles = multer({ storage: storage }).fields([
+    { name: "profilePicture", maxCount: 1 },
+    { name: "scopeOfWork", maxCount: 1 }
+]);
+
+export const uploadProviderFiles = multer({ storage: storage }).fields([
+    { name: "profilePicture", maxCount: 1 },
+    { name: "cvOrCompanyProfile", maxCount: 1 }
+]);
