b515f60f43e24f797a858a15ec64348f1fd8a087
YahyaNashar22 <yahyanashar22@gmail.com>
2025-01-11 20:57:37 +0200

    toggle functionality on approve quotatuion

--- routes/requestRoutes.js
+++ routes/requestRoutes.js
@@ -1,12 +1,14 @@
 import express from "express";
 
-import { createRequest, getAllRequests, passRequestToProvider } from "../controllers/requestControllers.js";
+import { approveQuotation, createRequest, getAllRequests, passRequestToProvider, sendBackToClient } from "../controllers/requestControllers.js";
 
 const requestRoutes = express.Router();
 
 requestRoutes.post("/create", createRequest);
 
 requestRoutes.patch("/pass-request-to-provider", passRequestToProvider);
+requestRoutes.patch("/approve-quotation", approveQuotation);
+requestRoutes.patch("/send-back-to-client", sendBackToClient);
 
 requestRoutes.get("/get-all", getAllRequests);
 
