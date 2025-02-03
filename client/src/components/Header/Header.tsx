51648f8cc215382b1264b05c99ef1c94c35921f5
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-03 22:37:42 +0200

    added header section with styles

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,11 +1,39 @@
 import styles from "./Header.module.css";
 
+import logo from "../../assets/icons/Logo.png";
+
 const Header = () => {
   return (
-    <head className={styles.wrapper}>
+    <header className={styles.wrapper}>
+      <div className={styles.left}>
+        <img src={logo} width={80} height={64} alt="logo" />
+        <ul className={styles.navLinks}>
+          <li className={styles.navLink}>
+            <a href="#join">Join</a>
+          </li>
+          <li className={styles.navLink}>
+            <a href="#about">About</a>
+          </li>
+          <li className={styles.navLink}>
+            <a href="#vision">Vision</a>
+          </li>
+          <li className={styles.navLink}>
+            <a href="#strategy">Strategy</a>
+          </li>
+          <li className={styles.navLink}>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </div>
 
-    </head>
-  )
-}
+      <div className={styles.phoneHelp}>
+        <p className={styles.helpText}>Need help? Talk to an expert</p>
+        <a className={styles.phone} href="https://wa.me/+961153425">
+          00 961 76 153 425
+        </a>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
