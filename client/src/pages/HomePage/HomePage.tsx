492a59f66602e600037e5b9a59369d0eb51e458e
YahyaNashar22 <yahyanashar22@gmail.com>
2025-02-06 22:09:04 +0200

    added quote section

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -2,12 +2,14 @@ import styles from "./HomePage.module.css";
 
 import Hero from "../../components/Hero/Hero";
 import About from "../../components/About/About";
+import Quote from "../../components/Quote/Quote";
 
 const HomePage = () => {
   return (
     <div className={styles.wrapper}>
       <Hero />
       <About />
+      <Quote />
     </div>
   );
 };
